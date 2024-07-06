58ec859381b566b8abe46888eab9ba8d1f4d2212
mohamed <mohamed@gmail.com>
2024-07-07 00:28:29 +0100

    exemple

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("test")
+// Définition de la fonction pour calculer la somme de deux nombres
+function calculerSomme(a, b) {
+    return a + b;
+}
+
+// Exemple d'utilisation de la fonction
+let nombre1 = 5;
+let nombre2 = 3;
+let resultat = calculerSomme(nombre1, nombre2);
+
+console.log(`La somme de ${nombre1} et ${nombre2} est égale à ${resultat}.`);
