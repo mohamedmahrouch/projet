3ad5441e9cc5ca29aced53394bbfc23753cac19f
mohamed <mohamed@gmail.com>
2024-07-12 23:29:56 +0100

    Changes

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello %s ", r.URL.Path[1:])
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello",helloHandler)
+	fmt.Fprintln(os.Stdout, "Server running on port 8088") // Message de confirmation
+	err := http.ListenAndServe(":8088", nil )
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
+		os.Exit(1)
+	}
+}
